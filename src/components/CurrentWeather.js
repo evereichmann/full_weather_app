f5cda232b4d25d9651c5754d40391b2d0c3794ba
evereichmann <eve.reichmann@gmail.com>
2022-01-15 11:59:26 -0600

    added geolocation to current weather

--- src/components/CurrentWeather.js
+++ src/components/CurrentWeather.js
@@ -1,4 +1,4 @@
-import React, { useState }  from 'react';
+import React, { useState, useEffect }  from 'react';
 import Navbar from './Navbar';
 
 function CurrentWeather() {
@@ -11,32 +11,67 @@ const api = {
 const [value, setValue] = useState('');
 const dataDiv = document.getElementById('weather-data');
 
-const search = () => { 
-    fetch(`${api.base}/weather?q=${value}&units=imperial&APPID=${api.key}`)
-      .then(resp => resp.json())
-      .then(weather => {
-        setValue('');
+useEffect(() => {
+  if ("geolocation" in navigator) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      const latitude = position.coords.latitude;
+      const longitude = position.coords.longitude;
+      async function getData() {
+        const response = await fetch(`${api.base}weather?lat=${latitude}&lon=${longitude}&appid=${api.key}`)
+        let actualData = await response.json();
         
+        const weather = actualData
+
+        console.log(weather)
+        
+        const dataDiv = document.getElementById('weather-data');
+
         dataDiv.innerHTML = (`
-            <h1>${Math.round(weather.main.temp)}°F</h1>
-            <h3>${Math.round(weather.main.temp_max)}°F</h3>
-            <h3>${Math.round(weather.main.temp_min)}°F</h3>
-            <img src='http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png'></img>
-            <h2>${weather.weather[0].description}</h2>
-        `);
-      }).catch( error => {
-        dataDiv.innerHTML = (`
-        <h1>Opps sorry, there was an error can you re-enter location</h1>
-        `);
+              <h1>${Math.round((weather.main.temp-273.15)*9/5+32)}°F</h1>
+              <h3>${Math.round((weather.main.temp_max-273.15)*9/5+32)}°F</h3>
+              <h3>${Math.round((weather.main.temp_min-273.15)*9/5+32)}°F</h3>
+              <img src='http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png'></img>
+              <h2>${weather.weather[0].description}</h2>
+          `);
+      }
+      getData()
       });
- }
+  }else{
 
+  }
+}, [])
 
+const search = () => { 
+  fetch(`${api.base}/weather?q=${value}&units=imperial&APPID=${api.key}`)
+    .then(resp => resp.json())
+    .then(weather => {
+      setValue('');
+      
+      dataDiv.innerHTML = (`
+          <h1>${Math.round(weather.main.temp)}°F</h1>
+          <h3>${Math.round(weather.main.temp_max)}°F</h3>
+          <h3>${Math.round(weather.main.temp_min)}°F</h3>
+          <img src='http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png'></img>
+          <h2>${weather.weather[0].description}</h2>
+      `);
+    }).catch( error => {
+      dataDiv.innerHTML = (`
+      <h1>Opps sorry, there was an error can you re-enter location</h1>
+      `);
+    });
+}
     return (
       <div className="CurrentWeather">
       <Navbar/>
-        <input placeholder='Location' type="text" className="search-bar" onChange={e => setValue(e.target.value)} value={value}/>
-        <button type="submit" onClick={search} >Search</button>
+        {("geolocation" in navigator)?(
+          <h3>geolocation is on data is loading 
+          </h3>
+        ):(
+          <div>
+            <input placeholder='Location' type="text" className="search-bar" onChange={e => setValue(e.target.value)} value={value}/>
+            <button type="submit" onClick={search} >Search</button>
+          </div>
+          )}
         <div id='weather-data'>
         </div> 
       </div>
